335005546044765766f1c7ff439362e119c80bb0
Alexandro <parashnoktf@gmail.com>
2021-01-25 13:10:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+app.use(express.json({extended: true}))
+
+const PORT = config.get('port') || 5000
+
+async function start () {
+	try {
+		await mongoose.connect(config.get('mongoUri'), {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true
+		})
+
+		app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+	} catch (e) {
+		console.error('[Server Error]', e)
+		process.exit(1)
+	}
+}
+
+start()
